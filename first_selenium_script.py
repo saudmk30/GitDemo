b519ed29e681252801cc269c34e2b6b9c93472da
saudmk30 <saud.arhamsoft@gmail.com>
2024-08-05 16:48:03 +0500

    Commit from the cloned project

--- first_selenium_script.py
+++ first_selenium_script.py
@@ -3,3 +3,5 @@ from selenium import webdriver
 driver = webdriver.Chrome()
 
 driver.get("http://demostore.supersqa.com")
+driver.get("http://demostore1.supersqa.com")
+driver.get("http://demostore2.supersqa.com")
